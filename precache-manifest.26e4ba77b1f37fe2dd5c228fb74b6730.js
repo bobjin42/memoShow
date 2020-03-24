5e3601980a9bbdaf9e8e84555fc9ea0367983013
Yicheng Jin <bobjin42@gmail.com>
2020-03-24 14:48:01 -0500

    Updates

---
+++ precache-manifest.26e4ba77b1f37fe2dd5c228fb74b6730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653caaf94ca539239c58b227f0c91f66",
+    "url": "/memoshow/index.html"
+  },
+  {
+    "revision": "02d17d8b1763846c8ccb",
+    "url": "/memoshow/static/css/main.1231a5ef.chunk.css"
+  },
+  {
+    "revision": "b8c3015d952c98f0dc18",
+    "url": "/memoshow/static/js/2.a8d4f595.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memoshow/static/js/2.a8d4f595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d17d8b1763846c8ccb",
+    "url": "/memoshow/static/js/main.2a46eb1d.chunk.js"
+  },
+  {
+    "revision": "aa58a22cbfd39b6aa288",
+    "url": "/memoshow/static/js/runtime-main.b4b32f4c.js"
+  }
+]);
